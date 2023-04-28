9e21b23a3c4b00a4f6d676bde893a579d4bd6b7e
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-28 13:25:26 +0900

    update

--- dataset/dataloader.py
+++ dataset/dataloader.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 df = pd.read_fwf('IAM/gt_test.txt', header=None)
 df.rename(columns={0: "file_name", 1: "text"}, inplace=True)
@@ -6,3 +7,9 @@ del df[2]
 
 # some file names end with jp instead of jpg, let's fix this
 df['file_name'] = df['file_name'].apply(lambda x: x + 'g' if x.endswith('jp') else x)
+
+
+train_df, test_df = train_test_split(df, test_size=0.2)
+# we reset the indices to start from zero
+train_df.reset_index(drop=True, inplace=True)
+test_df.reset_index(drop=True, inplace=True)
