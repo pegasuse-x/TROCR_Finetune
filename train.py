c8eb4e5e9dbefd28f912cfbafa4a89c9c536c15d
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-28 13:49:43 +0900

    update

--- train.py
+++ train.py
@@ -11,5 +11,17 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-stage1")
 model.to(device)
 
+# set special tokens used for creating the decoder_input_ids from the labels
+model.config.decoder_start_token_id = processor.tokenizer.cls_token_id
+model.config.pad_token_id = processor.tokenizer.pad_token_id
+# make sure vocab size is set correctly
+model.config.vocab_size = model.config.decoder.vocab_size
 
+# set beam search parameters
+model.config.eos_token_id = processor.tokenizer.sep_token_id
+model.config.max_length = 64
+model.config.early_stopping = True
+model.config.no_repeat_ngram_size = 3
+model.config.length_penalty = 2.0
+model.config.num_beams = 4
 
